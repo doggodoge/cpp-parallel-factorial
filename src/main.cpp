2ac1f2adb94ea3a4658a1a731fa6faea9a3708c8
Gary Moore <madmangaz@gmail.com>
2022-02-09 23:27:24 +0000

    working implementation of parallel factorial

--- src/main.cpp
+++ src/main.cpp
@@ -1,44 +1,95 @@
-#include <cstdio>
-#include <thread>
+#include <future>
+#include <gmp.h>
 #include <vector>
 
-void say_hello(std::string const &name);
-std::vector<std::thread> init_threads(std::vector<std::string> const &names);
-void join_threads(std::vector<std::thread> &threads);
+auto const processor_count = std::thread::hardware_concurrency();
 
-int main() {
-  std::vector<std::string> const names{
-      "First",
-      "Second",
-      "Third",
-      "Fourth",
-      "Fifth",
-      "Sixth",
-      "Seventh",
-      "Eighth",
-      "Ninth",
-      "Tenth"
-  };
-  std::vector<std::thread> threads = init_threads(names);
-  join_threads(threads);
+struct FactorialRange {
+  long start, end;
+  FactorialRange(long start, long end) {
+    this->start = start;
+    this->end = end;
+  }
+};
+
+std::vector<FactorialRange> get_ranges(long limit);
+std::string factorial_chunk(long start, long end);
+std::string
+arbitrary_precision_product(std::vector<std::string> const &str_numbers);
+std::vector<std::future<std::string>>
+init_futures(std::vector<FactorialRange> const &ranges);
+
+int main(int argc, char *argv[]) {
+  if (argc == 1) {
+    std::fprintf(stderr, "Please enter a valid number.\n");
+    return EXIT_FAILURE;
+  }
+
+  auto limit = std::strtol(argv[1], nullptr, 10);
+  std::vector<FactorialRange> ranges = get_ranges(limit);
+
+  auto futures = init_futures(ranges);
+  std::vector<std::string> results;
+  results.reserve(processor_count);
+  for (auto &future : futures) {
+    results.emplace_back(future.get());
+  }
+  std::string const result = arbitrary_precision_product(results);
+  std::printf("%s\n", result.c_str());
   return EXIT_SUCCESS;
 }
 
-void say_hello(std::string const &name) {
-  printf("Hello, %s!\n", name.c_str());
+std::vector<FactorialRange> get_ranges(long limit) {
+  if (limit < processor_count) {
+    return std::vector{FactorialRange(1, limit)};
+  }
+  std::vector<FactorialRange> ranges;
+  ranges.reserve(processor_count);
+  long const chunk_size = limit / processor_count + 1;
+  long start = 1;
+  for (auto i = 0; i < processor_count; i++) {
+    if (start + chunk_size > limit) {
+      ranges.emplace_back(FactorialRange(start, limit));
+    } else {
+      ranges.emplace_back(FactorialRange(start, start + chunk_size));
+    }
+    start += chunk_size + 1;
+  }
+  return ranges;
 }
 
-std::vector<std::thread> init_threads(std::vector<std::string> const &names) {
-  std::vector<std::thread> threads;
-  threads.reserve(names.size());
-  for (auto name : names) {
-    threads.emplace_back(std::thread(say_hello, name));
+std::string factorial_chunk(long start, long end) {
+  mpz_t acc;
+  mpz_init_set_ui(acc, 1);
+  for (long i = start; i <= end; i++) {
+    mpz_mul_ui(acc, acc, i);
   }
-  return threads;
+  auto result = std::string(mpz_get_str(nullptr, 10, acc));
+  mpz_clear(acc);
+  return result;
 }
 
-void join_threads(std::vector<std::thread> &threads) {
-  for (auto &thread : threads) {
-    thread.join();
+std::string
+arbitrary_precision_product(std::vector<std::string> const &str_numbers) {
+  mpz_t acc, operand;
+  mpz_init_set_ui(acc, 1);
+  mpz_init_set_ui(operand, 1);
+  for (auto const &number_str : str_numbers) {
+    mpz_set_str(operand, number_str.c_str(), 10);
+    mpz_mul(acc, acc, operand);
   }
+  auto result = std::string(mpz_get_str(nullptr, 10, acc));
+  mpz_clear(acc);
+  mpz_clear(operand);
+  return result;
+}
+
+std::vector<std::future<std::string>>
+init_futures(std::vector<FactorialRange> const &ranges) {
+  std::vector<std::future<std::string>> futures;
+  futures.reserve(ranges.size());
+  for (auto range : ranges) {
+    futures.emplace_back(std::async(factorial_chunk, range.start, range.end));
+  }
+  return futures;
 }
