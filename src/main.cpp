456a02352422b74352a96dce7929c5a3cf846e6e
Gary Moore <gary.moore1@ibm.com>
2022-02-11 16:02:33 +0000

    fix(main): use iostream instead of cstdio

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,14 @@
-#include <cstdio>
+#include <iostream>
 #include <string>
 #include "parallel_factorial.h"
 
 int main(int argc, char *argv[]) {
   if (argc == 1) {
-    std::fprintf(stderr, "Please enter a valid number.\n");
+    std::cerr << "Please enter a number.";
     return EXIT_FAILURE;
   }
   auto const limit = std::strtol(argv[1], nullptr, 10);
   std::string result = parallel_factorial(limit);
-  std::printf("%s\n", result.c_str());
+  std::cout << result << std::endl;
   return EXIT_SUCCESS;
 }
