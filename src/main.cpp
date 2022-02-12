f7470b1994e46ee14c9f664ea3f517e3158a6ed9
Gary Moore <gary.moore1@ibm.com>
2022-02-12 00:17:54 +0000

    Revert "fix(main): use iostream instead of cstdio"
    This reverts commit 456a02352422b74352a96dce7929c5a3cf846e6e.

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,14 @@
-#include <iostream>
+#include <cstdio>
 #include <string>
 #include "parallel_factorial.h"
 
 int main(int argc, char *argv[]) {
   if (argc == 1) {
-    std::cerr << "Please enter a number.";
+    std::fprintf(stderr, "Please enter a valid number.\n");
     return EXIT_FAILURE;
   }
   auto const limit = std::strtol(argv[1], nullptr, 10);
   std::string result = parallel_factorial(limit);
-  std::cout << result << std::endl;
+  std::printf("%s\n", result.c_str());
   return EXIT_SUCCESS;
 }
